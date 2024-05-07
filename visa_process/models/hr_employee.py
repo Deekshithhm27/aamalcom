4d733e8e1d38c69e2cfcbd73ea4b71dc118a6356
Deekshithhm27 <deekshith.h@lucidspire.com>
2024-05-07 21:39:44 +0530

    Fixed issue with Submit button for general query and name changes for fields

--- visa_process/models/hr_employee.py
+++ visa_process/models/hr_employee.py
@@ -66,6 +66,7 @@ class HrEmployee(models.Model):
     contract_warning = fields.Boolean(string='Contract Warning', store=True, compute='_compute_contract_warning', groups="hr.group_hr_user,visa_process.group_hr_employee,visa_process.group_hr_client")
 
     hr_agency_id = fields.Many2one('hr.agency',string="Agency")
+    country_of_birth = fields.Many2one('res.country', string="Issuance of Passport", groups="hr.group_hr_user", tracking=True)
 
 
     @api.model
