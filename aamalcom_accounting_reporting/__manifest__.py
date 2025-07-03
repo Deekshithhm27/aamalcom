e844d966d5def3b65f464e51060814cb098a3dd6
Deekshithhm27 <deekshith.h@lucidspire.com>
2025-07-03 19:16:35 +0530

    Rearranged data in manifest

--- aamalcom_accounting_reporting/__manifest__.py
+++ aamalcom_accounting_reporting/__manifest__.py
@@ -11,13 +11,13 @@
     'category': 'Accounting',
     'depends': ['account', 'accounting_pdf_reports','aamalcom_accounting','aamalcom_insurance'],
     'data': [
-        'data/mail_templates.xml',
         'reports/paperformat.xml',
         'reports/report_templates.xml',
         'reports/report_insurance_invoice.xml',
         'reports/report_invoice.xml',
         'reports/report_action.xml',
         'reports/report_journal_entries.xml',
+        'data/mail_templates.xml',
         'views/account_move_views.xml'
     ],
     'installable': True,
