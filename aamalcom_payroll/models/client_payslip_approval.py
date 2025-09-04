aa364ce144f811d2d1300f1c4a6d197b1dcc3244
Sangeetha0822 <sangeethab0822@gmail.com>
2025-09-04 11:21:25 +0530

    Date fields are added for docuemnt store

--- aamalcom_payroll/models/client_payslip_approval.py
+++ aamalcom_payroll/models/client_payslip_approval.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime, date
 
 class ClientPayslipApproval(models.Model):
     _name = 'client.payslip.approval'
@@ -16,15 +17,15 @@ class ClientPayslipApproval(models.Model):
 
     from_date = fields.Date(string="From Date", required=True, tracking=True)
     to_date = fields.Date(string="To Date", required=True, tracking=True)
-
+    create_date = fields.Datetime(string="Create Date", readonly=True, default=lambda self: datetime.now())
+    processed_date = fields.Datetime(string="Processed Date", readonly=True, tracking=True)
     approval_payslip_document = fields.Binary(string="Approval Payslip Document")
     approval_payslip_filename = fields.Char(string="Document Filename")
-    updated_approval_payslip_document = fields.Binary(string="Updated Approval Payslip Document")
-    reupdated_approval_payslip_document = fields.Binary(string="Re-Updtaed Approval Payslip Document")
-    other_payslip_document = fields.Binary(string="Other Document")
-    other_payslip_second_document = fields.Binary(string="Other Document")
-    other_payslip__third_document = fields.Binary(string="Other Document")
-    
+    payslip_documents_ids = fields.One2many(
+            comodel_name='client.payslip.document',
+            inverse_name='payslip_approval_id',
+            string='Updated Documents'
+        )
     # New field to store the refusal reason
     refusal_reason = fields.Text(string="Refusal Reason", tracking=True, readonly=True)
 
@@ -43,6 +44,19 @@ class ClientPayslipApproval(models.Model):
         string="Is Payroll Manager?",
         compute="_compute_is_payroll_manager"
     )
+    approval_document_date = fields.Date(string="Approval Document Date", compute="_compute_approval_document_date", store=True)
+
+    @api.depends('approval_payslip_document')
+    def _compute_approval_document_date(self):
+        """
+        Set the date when the approval document is uploaded.
+        """
+        for rec in self:
+            if rec.approval_payslip_document:
+                rec.approval_document_date = date.today()
+            else:
+                rec.approval_document_date = False
+            
 
     @api.depends()
     def _compute_is_payroll_manager(self):
@@ -56,36 +70,42 @@ class ClientPayslipApproval(models.Model):
         for rec in self:
             rec.state = 'submit_to_payroll'
             rec.message_post(body="Payslip approval submitted to Payroll.")
+            rec.processed_date = datetime.now()
         return True
 
     def action_submit_to_pm(self):
         for rec in self:
             rec.state = 'submit_to_pm'
             rec.message_post(body="Payslip updated and submitted to PM.")
+            rec.processed_date = datetime.now()
         return True
 
     def action_reviewed_by_pm(self):
         for rec in self:
             rec.state = 'verified_by_pm'
             rec.message_post(body="Payslip verified by PM.")
+            rec.processed_date = datetime.now()
         return True
 
     def action_submit_to_payroll_employee(self):
         for rec in self:
             rec.state = 'submit_to_payroll_employee'
             rec.message_post(body="Payslip submitted for Payroll Employee review.")
+            rec.processed_date = datetime.now()
         return True
 
     def action_reviewed_by_payroll_employee(self):
         for rec in self:
             rec.state = 'submit_to_hr_manager'
             rec.message_post(body="Payslip submitted to HR Manager for final review.")
+            rec.processed_date = datetime.now()
         return True
 
     def action_reviewed_by_hr_manager(self):
         for rec in self:
             rec.state = 'done'
             rec.message_post(body="Payslip process complete. The document is finalized.")
+            rec.processed_date = datetime.now()
         return True
 
     # This method is now a button to open the wizard, not change the state directly
