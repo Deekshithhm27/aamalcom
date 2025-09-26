66504fdca487164bb5f6bc400aa6e40201cce0fa
Sangeetha0822 <sangeethab0822@gmail.com>
2025-09-26 13:01:12 +0530

    Partner Ledger Excel Report

--- odoo_mates/accounting_pdf_reports/wizard/__init__.py
+++ odoo_mates/accounting_pdf_reports/wizard/__init__.py
@@ -9,3 +9,5 @@ from . import account_trial_balance
 from . import account_tax_report
 from . import aged_partner
 from . import account_journal_audit
+from . import trial_balance_xsl
+from . import partner_ledger_xsl
