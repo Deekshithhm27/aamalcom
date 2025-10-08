ecfde86ef65d6c911017b75a6ff8469e0d3414ee
Deekshithhm27 <deekshith.h@lucidspire.com>
2025-10-09 03:33:04 +0530

    Updated invoice attachment for send and print button inside wizard

--- aamalcom_accounting_reporting/models/account_move.py
+++ aamalcom_accounting_reporting/models/account_move.py
@@ -111,6 +111,19 @@ class AccountMove(models.Model):
             self.action_save_merged_insurance_pdf()
         return super(AccountMove, self).action_post()
 
+    def action_invoice_print(self):
+        """ Print the invoice and mark it as sent, so that we can see more
+            easily the next step of the workflow
+        """
+        if any(not move.is_invoice(include_receipts=True) for move in self):
+            raise UserError(_("Only invoices could be printed."))
+
+        self.filtered(lambda inv: not inv.is_move_sent).write({'is_move_sent': True})
+        if self.user_has_groups('account.group_account_invoice'):
+            return self.env.ref('aamalcom_accounting_reporting.action_report_tax_invoice').report_action(self)
+        else:
+            return self.env.ref('account.account_invoices_without_payment').report_action(self)
+
 
     def _get_mail_template(self):
         """
