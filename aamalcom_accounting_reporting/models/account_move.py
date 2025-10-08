4d0f9b797e5d593b787810c3e02d6909378ad285
Deekshithhm27 <deekshith.h@lucidspire.com>
2025-10-09 02:50:21 +0530

    Updated journal reports

--- aamalcom_accounting_reporting/models/account_move.py
+++ aamalcom_accounting_reporting/models/account_move.py
@@ -109,3 +109,14 @@ class AccountMove(models.Model):
         if self.invoice_type == 'insurance':
             self.action_save_merged_insurance_pdf()
         return super(AccountMove, self).action_post()
+
+
+    def _get_mail_template(self):
+        """
+        :return: the correct mail template based on the current move type
+        """
+        return (
+            'account.email_template_edi_credit_note'
+            if all(move.move_type == 'out_refund' for move in self)
+            else 'aamalcom_accounting_reporting.action_report_tax_invoice'
+        )
